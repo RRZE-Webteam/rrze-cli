865b35a0bb4e00f44908177897d2819c94ca15c8
rvdforst <rolf.v.d.forst@fau.de>
2023-01-30 10:48:10 +0100

    Create Info.php

---
+++ includes/Info.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RRZE\CLI;
+
+defined('ABSPATH') || exit;
+
+use WP_CLI_Command;
+use function RRZE\CLI\plugin;
+
+/**
+ * @package RRZE\CLI
+ */
+class Info extends WP_CLI_Command
+{
+    /**
+     * Displays General Info about RRZE-CLI
+     *
+     * @param array $args
+     * @param array $assoc_args
+     */
+    public function __invoke($args, $assoc_args)
+    {
+        \cli\line('RRZE-CLI version: %Yv' . plugin()->getVersion() . '%n');
+        \cli\line();
+    }
+}
