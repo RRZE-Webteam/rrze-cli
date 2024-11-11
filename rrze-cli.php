37f2d335ae8143fc44254b6cbcbf0fd4be3f692a
Rolf <rolf.v.d.forst@fau.de>
2024-11-11 17:23:39 +0100

    Update rrze-cli.php

--- rrze-cli.php
+++ rrze-cli.php
@@ -1,140 +1,157 @@
 <?php
+
 /*
-Plugin Name:      RRZE CLI
-Plugin URI:       https://github.com/RRZE-Webteam/rrze-cli
-Description:      A set of WP-CLI commands to help manage a WordPress website.
-Version:          1.0.2
-Author:           RRZE-Webteam
-Author URI:       https://blogs.fau.de/webworking/
-License:          GNU General Public License v2
-License URI:      http://www.gnu.org/licenses/gpl-2.0.html
-Domain Path:      /languages
-Text Domain:      rrze-cli
+Plugin Name:        RRZE CLI
+Plugin URI:         https://github.com/RRZE-Webteam/rrze-cli
+Version:            1.0.2
+Description:        A set of WP-CLI commands to help manage a WordPress website.
+Author:             RRZE-Webteam
+Author URI:         https://blogs.fau.de/webworking/
+License:            GNU General Public License v2
+License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:        rrze-cli
+Domain Path:        /languages
+Requires at least:  6.6
+Requires PHP:       8.2
 */
 
 namespace RRZE\CLI;
 
 defined('ABSPATH') || exit;
 
-const RRZE_PHP_VERSION = '8.1';
-const RRZE_WP_VERSION  = '6.4';
-
 // Autoloader
 require_once 'vendor/autoload.php';
 
-register_activation_hook(__FILE__, __NAMESPACE__ . '\activation');
-register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
-
+/**
+ * Add an action hook for the 'plugins_loaded' hook.
+ *
+ * This code hooks into the 'plugins_loaded' action hook to execute a callback function when
+ * WordPress has fully loaded all active plugins and the theme's functions.php file.
+ */
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
 /**
- * loadTextdomain
+ * Load the text domain for plugin localization.
+ *
+ * This method loads the translation files for the plugin.
  */
 function loadTextdomain()
 {
+    // Load the plugin's text domain for localization.
     load_plugin_textdomain(
-        'rrze-cli',
-        false,
-        sprintf('%s/languages/', dirname(plugin_basename(__FILE__)))
+        'rrze-cli', // The text domain used for translating plugin strings.
+        false, // Deprecated. Should be set to 'false'.
+        sprintf('%s/languages/', dirname(plugin_basename(__FILE__))) // The path to the translation files.
     );
 }
 
 /**
- * System requirements verification.
- * @return string Return an error message.
- */
-function systemRequirements(): string
-{
-    global $wp_version;
-    // Strip off any -alpha, -RC, -beta, -src suffixes.
-    list($wpVersion) = explode('-', $wp_version);
-    $phpVersion = phpversion();
-    $error = '';
-    if (!is_php_version_compatible(RRZE_PHP_VERSION)) {
-        $error = sprintf(
-            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-cli'),
-            $phpVersion,
-            RRZE_PHP_VERSION
-        );
-    } elseif (!is_wp_version_compatible(RRZE_WP_VERSION)) {
-        $error = sprintf(
-            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-cli'),
-            $wpVersion,
-            RRZE_WP_VERSION
-        );
-    }
-    return $error;
-}
-
-/**
- * Activation callback function.
- */
-function activation()
-{
-    loadTextdomain();
-    if ($error = systemRequirements()) {
-        deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(
-            sprintf(
-                /* translators: 1: The plugin name, 2: The error string. */
-                __('Plugins: %1$s: %2$s', 'rrze-cli'),
-                plugin_basename(__FILE__),
-                $error
-            )
-        );
-    }
-}
-
-/**
- * Deactivation callback function.
- */
-function deactivation()
-{
-    // Nothing to do.
-}
-
-/**
- * Instantiate Plugin class.
- * @return object Plugin
+ * Singleton pattern for initializing and accessing the main plugin instance.
+ *
+ * This method ensures that only one instance of the Plugin class is created and returned.
+ *
+ * @return Plugin The main instance of the Plugin class.
  */
 function plugin()
 {
+    // Declare a static variable to hold the instance.
     static $instance;
+
+    // Check if the instance is not already created.
     if (null === $instance) {
+        // Add a new instance of the Plugin class, passing the current file (__FILE__) as a parameter.
         $instance = new Plugin(__FILE__);
     }
+
+    // Return the main instance of the Plugin class.
     return $instance;
 }
 
 /**
- * Execute on 'plugins_loaded' API/action.
- * @return void
+ * Check system requirements for the plugin.
+ *
+ * This method checks if the server environment meets the minimum WordPress and PHP version requirements
+ * for the plugin to function properly.
+ *
+ * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ */
+function systemRequirements(): string
+{
+    // Get the global WordPress version.
+    global $wp_version;
+
+    // Get the PHP version.
+    $phpVersion = phpversion();
+
+    // Initialize an error message string.
+    $error = '';
+
+    // Check if the WordPress version is compatible with the plugin's requirement.
+    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-updater'),
+            $wp_version,
+            plugin()->getRequiresWP()
+        );
+    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
+        // Check if the PHP version is compatible with the plugin's requirement.
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-updater'),
+            $phpVersion,
+            plugin()->getRequiresPHP()
+        );
+    }
+
+    // Return the error message string, which will be empty if requirements are satisfied.
+    return $error;
+}
+
+/**
+ * Handle the loading of the plugin.
+ *
+ * This function is responsible for initializing the plugin, loading text domains for localization,
+ * checking system requirements, and displaying error notices if necessary.
  */
 function loaded()
 {
+    // Load the plugin's text domain for localization.
     loadTextdomain();
+
+    // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
+
+    // Check system requirements and store any error messages.
     if ($error = systemRequirements()) {
+        // If there is an error, add an action to display an admin notice with the error message.
         add_action('admin_init', function () use ($error) {
+            // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                $pluginData = get_plugin_data(plugin()->getFile());
-                $pluginName = $pluginData['Name'];
+                // Get plugin data to retrieve the plugin's name.
+                $pluginName = plugin()->getName();
+
+                // Determine the admin notice tag based on network-wide activation.
                 $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+
+                // Add an action to display the admin notice.
                 add_action($tag, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            __('Plugins: %1$s: %2$s', 'rrze-cli') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-updater') .
                             '</p></div>',
-                        esc_html($pluginName),
-                        esc_html($error)
+                        $pluginName,
+                        $error
                     );
                 });
             }
         });
+
+        // Return to prevent further initialization if there is an error.
         return;
     }
+
+    // If there are no errors, create an instance of the 'Main' class.
     new Main;
 }
