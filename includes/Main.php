d3cccb70be74f6235a50a2436ebc70d512d8c7eb
Rolf <rolf.v.d.forst@fau.de>
2025-02-19 15:40:24 +0100

    Update Main.php

--- includes/Main.php
+++ includes/Main.php
@@ -9,12 +9,13 @@ use WP_CLI;
 class Main
 {
     /**
-     * __construct
+     * @return void
+     * @throws WP_CLI\ExitException
      */
-    public function __construct()
+    public function loaded(): void
     {
         // Check if WP_CLI is available
-        if (!class_exists('WP_CLI')) {
+        if (!defined('WP_CLI') || !WP_CLI) {
             return;
         }
 
@@ -22,24 +23,33 @@ class Main
             WP_CLI::error(__('This command must be run on a multisite installation.', 'rrze-cli'));
         }
 
-        // Get the current working directory
-        $current_dir = getcwd();
+        // Output the WP_CLI version
+        $wpcliVersion = WP_CLI::runcommand('cli version', ['return' => true]);
+        WP_CLI::log(trim($wpcliVersion));
+
+        // Get the current working directoryclear
+        $currentDir = getcwd();
+        if (false === $currentDir) {
+            WP_CLI::error(__('Unable to determine the current working directory.', 'rrze-cli'));
+        }
 
         // Output the current directory
-        WP_CLI::log("Current directory: " . $current_dir);
+        WP_CLI::log("Current directory: " . $currentDir);
 
         // Check if the command is running in the main directory of WordPress
-        if (!file_exists($current_dir . '/wp-load.php')) {
+        if (!file_exists($currentDir . '/wp-load.php')) {
             WP_CLI::error(__('This command must be run from the main directory of a WordPress installation.', 'rrze-cli'));
         }
 
         // rrze-cli
         WP_CLI::add_command('rrze-cli info', __NAMESPACE__ . '\\Info');
+
         // rrze-migration
         WP_CLI::add_command('rrze-migration export', __NAMESPACE__ . '\\Migration\\Export');
         WP_CLI::add_command('rrze-migration import', __NAMESPACE__ . '\\Migration\\Import');
         WP_CLI::add_command('rrze-migration posts', __NAMESPACE__ . '\\Migration\\Posts');
-        // rrze-cli
-        WP_CLI::add_command('rrze-multilang postmeta', __NAMESPACE__ . '\\Multilang\\PostMeta');        
+
+        // rrze-multilang
+        WP_CLI::add_command('rrze-multilang migration', __NAMESPACE__ . '\\Multilang\\Migration');
     }
 }
