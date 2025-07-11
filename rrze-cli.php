536873434aeec52afbc0a4cd5c7966063a37e720
Rolf <rolf.v.d.forst@fau.de>
2025-07-11 11:56:17 +0200

    Update rrze-cli.php

--- rrze-cli.php
+++ rrze-cli.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE CLI
 Plugin URI:         https://github.com/RRZE-Webteam/rrze-cli
-Version:            1.2.5
+Version:            1.2.6
 Description:        A set of WP-CLI commands to help manage a WordPress website.
 Author:             RRZE-Webteam
 Author URI:         https://blogs.fau.de/webworking/
@@ -11,7 +11,7 @@ License:            GNU General Public License v2
 License URI:        http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:        rrze-cli
 Domain Path:        /languages
-Requires at least:  6.7
+Requires at least:  6.8
 Requires PHP:       8.2
 */
 
@@ -22,16 +22,39 @@ defined('ABSPATH') || exit;
 // Composer autoloader
 require_once 'vendor/autoload.php';
 
-// Load the plugin's text domain for localization.
-add_action('init', fn() => load_plugin_textdomain('rrze-cli', false, dirname(plugin_basename(__FILE__)) . '/languages'));
+// Register activation hook for the plugin
+register_activation_hook(__FILE__, __NAMESPACE__ . '\activation');
+
+// Register deactivation hook for the plugin
+register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
 
 /**
  * Add an action hook for the 'plugins_loaded' hook.
  *
- * This code hooks into the 'plugins_loaded' action hook to execute a callback function when
- * WordPress has fully loaded all active plugins and the theme's functions.php file.
+ * This hook is triggered after all active plugins have been loaded, allowing the plugin to perform
+ * initialization tasks.
  */
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
+
+/**
+ * Activation callback function.
+ * 
+ * @return void
+ */
+function activation()
+{
+    // Use this if you need to perform tasks on activation.
+}
+
+/**
+ * Deactivation callback function.
+ * 
+ * @return void
+ */
+function deactivation()
+{
+    // Use this if you need to perform tasks on deactivation.
+}
 
 /**
  * Singleton pattern for initializing and accessing the main plugin instance.
@@ -56,44 +79,17 @@ function plugin()
 }
 
 /**
- * Check system requirements for the plugin.
- *
- * This method checks if the server environment meets the minimum WordPress and PHP version requirements
- * for the plugin to function properly.
- *
- * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ * Callback function to load the plugin textdomain.
+ * 
+ * @return void
  */
-function systemRequirements(): string
+function load_textdomain()
 {
-    // Get the global WordPress version.
-    global $wp_version;
-
-    // Get the PHP version.
-    $phpVersion = phpversion();
-
-    // Initialize an error message string.
-    $error = '';
-
-    // Check if the WordPress version is compatible with the plugin's requirement.
-    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
-        $error = sprintf(
-            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-            __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-settings'),
-            $wp_version,
-            plugin()->getRequiresWP()
-        );
-    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
-        // Check if the PHP version is compatible with the plugin's requirement.
-        $error = sprintf(
-            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-            __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-settings'),
-            $phpVersion,
-            plugin()->getRequiresPHP()
-        );
-    }
-
-    // Return the error message string, which will be empty if requirements are satisfied.
-    return $error;
+    load_plugin_textdomain(
+        'rrze-cli',
+        false,
+        dirname(plugin_basename(__FILE__)) . '/languages'
+    );
 }
 
 /**
@@ -101,30 +97,59 @@ function systemRequirements(): string
  *
  * This function is responsible for initializing the plugin, loading text domains for localization,
  * checking system requirements, and displaying error notices if necessary.
+ * 
+ * @return void
  */
 function loaded()
 {
     // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
 
-    // Check system requirements and store any error messages.
-    if ($error = systemRequirements()) {
-        // If there is an error, add an action to display an admin notice with the error message.
-        add_action('admin_init', function () use ($error) {
+    // Load the plugin textdomain for translations.
+    add_action(
+        'init',
+        __NAMESPACE__ . '\load_textdomain'
+    );
+
+    // Check system requirements.
+    if (
+        ! $wpCompatibe = is_wp_version_compatible(plugin()->getRequiresWP())
+            || ! $phpCompatible = is_php_version_compatible(plugin()->getRequiresPHP())
+    ) {
+        // If the system requirements are not met, add an action to display an admin notice.
+        add_action('init', function () use ($wpCompatibe, $phpCompatible) {
             // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                // Get plugin data to retrieve the plugin's name.
+                // Determine the appropriate admin notice tag based on whether the plugin is network activated.
+                $hookName = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+
+                // Get the plugin name for display in the admin notice.
                 $pluginName = plugin()->getName();
 
-                // Determine the admin notice tag based on network-wide activation.
-                $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+                $error = '';
+                if (! $wpCompatibe) {
+                    $error = sprintf(
+                        /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+                        __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-cli'),
+                        wp_get_wp_version(),
+                        plugin()->getRequiresWP()
+                    );
+                } elseif (! $phpCompatible) {
+                    $error = sprintf(
+                        /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+                        __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-cli'),
+                        PHP_VERSION,
+                        plugin()->getRequiresPHP()
+                    );
+                }
 
-                // Add an action to display the admin notice.
-                add_action($tag, function () use ($pluginName, $error) {
+                // Display the error notice in the admin area.
+                // This will show a notice with the plugin name and the error message.
+                add_action($hookName, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            esc_html__('Plugins: %1$s: %2$s', 'rrze-settings') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-cli') .
                             '</p></div>',
                         $pluginName,
                         $error
@@ -133,10 +158,11 @@ function loaded()
             }
         });
 
-        // Return to prevent further initialization if there is an error.
+        // If the system requirements are not met, the plugin initialization will not proceed.
         return;
     }
 
-    // If there are no errors, create an instance of the 'Main' class and trigger its 'loaded' method.
+    // If system requirements are met, proceed to initialize the main plugin instance.
+    // This will load the main functionality of the plugin.
     (new Main)->loaded();
 }
